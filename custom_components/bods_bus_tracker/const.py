ee0f3d7dae450e2fdb0d9b2e84ed471c55d4a796
Iain Hay <iain@demig.co.uk>
2026-09-13 09:23:11 +0100

    Prepare stable v0.6.0 runtime version

--- custom_components/bods_bus_tracker/const.py
+++ custom_components/bods_bus_tracker/const.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 DOMAIN = "bods_bus_tracker"
-VERSION = "0.6.0-beta.7"
+VERSION = "0.6.0"
 PLATFORMS = ["sensor", "binary_sensor"]
 SUBENTRY_TYPE_STOP = "stop"
 
